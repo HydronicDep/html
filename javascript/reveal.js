f672f73d6eb6ffc374a1957dda9bda771922e345
HydronicDep <153455925+HydronicDep@users.noreply.github.com>
2023-12-21 13:24:58 -0300

    Paste Website/base into Github

---
+++ javascript/reveal.js
@@ -0,0 +1,20 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito-base1', {
+    duration: 1000,
+    distance: '25px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-base2', {
+    duration: 1000,
+    distance: '25px',
+    origin: 'right'
+})
+
+revelar.reveal('.efeito-base3', {
+    duration: 1000,
+    distance: '25px',
+    origin: 'top'
+})
